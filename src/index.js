92c1a23cf6bf11b64ceafbe68308f6f06aa0abb8
Ting <heng.nguan.ting@intel.com>
2022-06-26 17:12:31 +0800

    trial2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div class="container">
+      <section class="one">
+        <h1>
+          First Page
+        </h1>
+      </section>
+      <section class="two">
+        <h1>
+          2nd Page
+        </h1>
+      </section>
+      <section class="three">
+        <h1>
+          Third Page
+        </h1>
+      </section>
+    </div>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
